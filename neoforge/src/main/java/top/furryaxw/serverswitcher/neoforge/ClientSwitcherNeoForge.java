c51fdc550a3d6f6c1d1347f18b5e42319153c045
Axw_0 <93119225+furryaxw@users.noreply.github.com>
2026-05-14 02:32:49 +0800

    Fix: remove EventBuses import (not available in Architectury 13.x for NeoForge)

--- neoforge/src/main/java/top/furryaxw/serverswitcher/neoforge/ClientSwitcherNeoForge.java
+++ neoforge/src/main/java/top/furryaxw/serverswitcher/neoforge/ClientSwitcherNeoForge.java
@@ -1,6 +1,5 @@
 package top.furryaxw.serverswitcher.neoforge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
@@ -9,7 +8,6 @@ import top.furryaxw.serverswitcher.ClientSwitcher;
 @Mod(ClientSwitcher.MOD_ID)
 public class ClientSwitcherNeoForge {
     public ClientSwitcherNeoForge(IEventBus modEventBus) {
-        EventBuses.registerModEventBus(ClientSwitcher.MOD_ID, modEventBus);
         modEventBus.addListener(this::onClientSetup);
     }
 
